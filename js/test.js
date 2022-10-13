15753d2982e60bd16feb1c6e42aa1a10ffb4c0b2
Oliver <oliver.huang@mail.utoronto.ca>
2022-10-13 17:53:53 -0400

    week 3 todo

--- js/test.js
+++ js/test.js
@@ -1,5 +1,12 @@
 // Note: for animation and fancy styling, https://www.amcharts.com/demos/variable-radius-pie-chart/ is an library to enhance the visualization for pie chart that I found, but not sure if I can use it
 
+
+//1. wedges are stacks sequancially in time
+//2. think about the math to implement the area of each slice (from outer to inner if overlap)
+//3. tooltip should include percentage of catagory of the current year (number), curr year, value of that year (price)
+//4. if click on the wedge of a year, change to that year's chart
+
+
 function read_csv(){
 
 }
@@ -12,6 +19,121 @@ function count_freq(){
 // generate/randomize color and assign them to each of the catogorical variables
 function generate_color(){
 
+}
+
+// 
+function generate_current_data(radius, summary, length){
+    var currLabelList=[];
+    //generate label list
+    for(var i=0; i<radius.length; i++){
+        currLabelList.push(radius[i]["label"]);
+    }
+
+    var dataList = [];
+    for(var i=0; i<length; i++){
+        var currStats=[];
+        //select the indexes
+        for(var j=0; j<summary.length; j++){
+            if(typeof summary[j][i] !== 'undefined' && typeof radius[i] !== 'undefined'){
+                currStats.push(summary[j][i]);
+            }
+        }
+        for(var k=0; k<currStats.length; k++){
+            var index = radius.findIndex(item => item["label"] === currStats[k]["label"]);
+            if(typeof radius[index] !== "undefined"){
+                currStats[k]["value"] = radius[index].value;
+            }
+        }
+        for(var k=0; k<currStats.length; k++){
+            if(currStats[k]?.value == undefined){
+                currStats.splice(k, 1);
+            }
+        }
+        var currStatsLabelList = [];
+        for(var k=0; k<currStats.length; k++){
+            currStatsLabelList.push(currStats[i]["label"]);
+        }
+        
+        dataList.push(currStats)
+    }
+    return dataList
+}
+
+//summarize the data the return the below format
+function summarize_data(){
+
+    sorted_data = [
+        [
+            {label: '1 Bedroom', time: 2007, radius: 77.85714285714286},
+            {label: '1 Bedroom', time: 2008, radius: 90.47619047619048},
+        ],
+        [
+            {label: '2 Bedrooms', time: 2008, radius: 86.48809523809524},
+            {label: '2 Bedrooms', time: 2010, radius: 88.69047619047619},
+            {label: '2 Bedrooms', time: 2009, radius: 100}
+        ],
+        [
+            {label: '3 Bedrooms', time: 2007, radius: 80.21164285714286},
+            {label: '3 Bedrooms', time: 2008, radius: 92.83333333333333},
+            {label: '3 Bedrooms', time: 2009, radius: 93.44047619047619},
+            {label: '3 Bedrooms', time: 2010, radius: 101.9672619047619},
+            {label: '3 Bedrooms', time: 2011, radius: 108.42771428571429},
+            {label: '3 Bedrooms', time: 2012, radius: 109.514}
+        ],
+        [               
+            {label: '4 Bedrooms', time: 2011, radius: 127.11309523809524},
+            {label: '4 Bedrooms', time: 2009, radius: 128.12352380952382},
+            {label: '4 Bedrooms', time: 2010, radius: 137.63392857142858},
+            {label: '4 Bedrooms', time: 2008, radius: 141.60257142857145},
+            {label: '4 Bedrooms', time: 2012, radius: 151.86147619047617},
+            {label: '4 Bedrooms', time: 2007, radius: 176.54761904761904}
+        ],
+        [
+            {label: '5 Bedrooms', time: 2011, radius: 133.33333333333334},
+            {label: '5 Bedrooms', time: 2008, radius: 145.23809523809524},
+            {label: '5 Bedrooms', time: 2009, radius: 178.57142857142858},
+            {label: '5 Bedrooms', time: 2007, radius: 190.47619047619048},
+            {label: '5 Bedrooms', time: 2010, radius: 194.36507142857144},
+            {label: '5 Bedrooms', time: 2012, radius: 241.66666666666666}
+        ],
+    ]
+    summary = [
+        [
+            {label: '1 Bedroom', time: 2007, inner: 0, outer: 77.85714285714286, color: "rgb(100, 50, 0)"},
+            {label: '1 Bedroom', time: 2008, inner: 77.85714285714286, outer: 90.47619047619048, color: "rgb(100, 50, 0)"},
+        ],
+        [
+            {label: '2 Bedrooms', time: 2008, inner: 0, outer: 86.48809523809524, color: "rgb(255, 0, 0)"},
+            {label: '2 Bedrooms', time: 2010, inner: 86.48809523809524, outer: 88.69047619047619, color: "rgb(255, 0, 0)"},
+            {label: '2 Bedrooms', time: 2009, inner: 88.69047619047619, outer: 100, color: "rgb(255, 0, 0)"}
+        ],
+        [
+            {label: '3 Bedrooms', time: 2007, inner: 0, outer: 80.21164285714286, color: "rgb(0, 50, 50)"},
+            {label: '3 Bedrooms', time: 2008, inner: 80.21164285714286, outer: 92.83333333333333, color: "rgb(0, 50, 50)"},
+            {label: '3 Bedrooms', time: 2009, inner: 92.83333333333333, outer: 93.44047619047619, color: "rgb(0, 50, 50)"},
+            {label: '3 Bedrooms', time: 2010, inner: 93.44047619047619, outer: 101.9672619047619, color: "rgb(0, 50, 50)"},
+            {label: '3 Bedrooms', time: 2011, inner: 101.9672619047619, outer: 108.42771428571429, color: "rgb(0, 50, 50)"},
+            {label: '3 Bedrooms', time: 2012, inner: 108.42771428571429, outer: 109.514, color: "rgb(0, 50, 50)"}
+        ],
+        [               
+            {label: '4 Bedrooms', time: 2011, inner: 0, outer: 127.11309523809524, color: "rgb(255, 100, 0)"},
+            {label: '4 Bedrooms', time: 2009, inner: 127.11309523809524, outer: 128.12352380952382, color: "rgb(255, 100, 0)"},
+            {label: '4 Bedrooms', time: 2010, inner: 137.63392857142858, outer: 137.63392857142858, color: "rgb(255, 100, 0)"},
+            {label: '4 Bedrooms', time: 2008, inner: 141.60257142857145, outer: 141.60257142857145, color: "rgb(255, 100, 0)"},
+            {label: '4 Bedrooms', time: 2012, inner: 151.86147619047617, outer: 151.86147619047617, color: "rgb(255, 100, 0)"},
+            {label: '4 Bedrooms', time: 2007, inner: 176.54761904761904, outer: 176.54761904761904, color: "rgb(255, 100, 0)"}
+        ],
+        [
+            {label: '5 Bedrooms', time: 2011, inner: 0, outer: 133.33333333333334, color: "rgb(0, 0, 0)"},
+            {label: '5 Bedrooms', time: 2008, inner: 133.33333333333334, outer: 145.23809523809524, color: "rgb(0, 0, 0)"},
+            {label: '5 Bedrooms', time: 2009, inner: 145.23809523809524, outer: 178.57142857142858, color: "rgb(0, 0, 0)"},
+            {label: '5 Bedrooms', time: 2007, inner: 178.57142857142858, outer: 190.47619047619048, color: "rgb(0, 0, 0)"},
+            {label: '5 Bedrooms', time: 2010, inner: 190.47619047619048, outer: 194.36507142857144, color: "rgb(0, 0, 0)"},
+            {label: '5 Bedrooms', time: 2012, inner: 194.36507142857144, outer: 241.66666666666666, color: "rgb(0, 0, 0)"}
+        ],
+    ]
+
+    return summary
 }
 
 // just a hard coded summary of some dummy data from sample_data.csv
@@ -123,7 +245,8 @@ var config =
                 }
             }
 
-            var unfiltered = graphData.cols[i].stats.filter(function(x) { return x.year != year;})
+            var unfiltered = graphData.cols[i].stats
+            // .filter(function(x) { return x.year != year;})
             console.log(unfiltered);
             for (var j = 0; j < unfiltered.length; j++)
             {
@@ -138,9 +261,9 @@ var config =
         for (var i = 0; i < graphData.rows.length; i++)
         {   
             var currYear = graphData.rows[i];
-            if(currYear == year){
-                continue;
-            }
+            // if(currYear == year){
+            //     continue;
+            // }
             
             var currStats = {
                 time: currYear, 
@@ -170,9 +293,8 @@ var config =
         console.log(combinedStats);
         console.log(dataSet);
 
-        //cleanup the dataset combined Stats with radius of 0 ?
 
-        //is is nessary ?
+        
 
 
 
@@ -214,12 +336,18 @@ var config =
         var radius = dataSet.radius;
 
         console.log(radius);
+        var summary = summarize_data();
+        console.log(summary);
 
-        
+        var data = generate_current_data(radius, summary, graphData.rows.length);
+        console.log(data);
+
         var arc = d3.svg.arc()
-        .innerRadius(0)
+        .innerRadius(function (d){
+            return d.data.inner
+        })
         .outerRadius(function (d) { 
-            return d.data.radius
+            return d.data.outer
         });
         
         
@@ -231,34 +359,22 @@ var config =
             
         var svg =  d3.select('.pieChartSvg').append('svg').attr('width',width).attr('height',height)
         
-        svg.selectAll('path')
-        .data(pie(radius))
-        .enter()
-        .append('path')
-        .attr('d',arc)
-        .attr('transform','translate(250,250)')
-        .attr("fill", function(d) {
-            return d.data.color;
-        })
-        .attr('stroke','white')
-        .style("opacity", 0.8)
-        .on("mouseover", function (d) {
-            d3.select("#tooltip")
-            .style("left", d3.event.pageX + "px")
-            .style("top", d3.event.pageY + "px")
-            .style("opacity", 1)
-            .select("#value")
-            .text(d.value);
-        })
-        .on("mouseout", function () {
-        // Hide the tooltip
-            d3.select("#tooltip")
-            .style("opacity", 0);;
-        });
+        // console.log(data[0]);
+        // svg.selectAll('path')
+        // .data(pie(data[0]))
+        // .enter()
+        // .append('path')
+        // .attr('d',arc)
+        // .attr('transform','translate(250,250)')
+        // .attr("fill", function(d) {
+        //     return d.data.color;
+        // })
+        // .attr('stroke','white')
+        // .style("opacity", 0.8)
 
         for(var i=0; i<combinedStats.length; i++){
             svg.selectAll("arc")
-            .data(pie(combinedStats[i].data))
+            .data(pie(data[i]))
             .enter()
             .append('path')
             .attr('d',arc)
@@ -267,35 +383,9 @@ var config =
                 return d.data.color;
             })
             .attr('stroke','white')
-            .style("opacity", 0.4)
-            .on("mouseover", function (d) {
-                d3.select("#tooltip")
-                .style("left", d3.event.pageX + "px")
-                .style("top", d3.event.pageY + "px")
-                .style("opacity", 1)
-                .select("#value")
-                .text(d.value);
-            })
-            .on("mouseout", function () {
-            // Hide the tooltip
-                d3.select("#tooltip")
-                .style("opacity", 0);;
-            });
-        }
+            .style("opacity", 0.8)
 
-        // svg.selectAll('text')
-        // .data(pie(radius))
-        // .enter()
-        // .append('text')
-        // .attr("transform", function(d) { 
-        //     return "translate(" + arc.centroid(d) + ")"; 
-        // })
-        // .text(function(d){ 
-        //     console.log(d.data.value);
-        //     return d.data.value;
-        // })
-        // .style("text-anchor", "middle")
-        // .style("font-size", 17)
+        }
         
         legend
         .selectAll("rect")

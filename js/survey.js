9b1c4b7b4fbe5b0dfd9a152d27cd94b07ead3063
Oliver Huang <oliver.h0803@gmail.com>
2022-12-05 21:54:39 -0500

    added survey charts

---
+++ js/survey.js
@@ -0,0 +1,839 @@
+data = [
+    {
+        "Year": 2007,
+        "price": "525000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2007,
+        "price": "290000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2007,
+        "price": "328000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2007,
+        "price": "380000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2007,
+        "price": "310000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2007,
+        "price": "465000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2007,
+        "price": "399000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2007,
+        "price": "1530000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2007,
+        "price": "359000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2007,
+        "price": "320000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2007,
+        "price": "385000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2007,
+        "price": "305000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2007,
+        "price": "850000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2007,
+        "price": "765000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2007,
+        "price": "517000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2007,
+        "price": "800000",
+        "bedrooms\r": "5\r"
+    },
+    {
+        "Year": 2007,
+        "price": "336000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2007,
+        "price": "535000",
+        "bedrooms\r": "5\r"
+    },
+    {
+        "Year": 2007,
+        "price": "900000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2007,
+        "price": "327000",
+        "bedrooms\r": "1\r"
+    },
+    {
+        "Year": 2008,
+        "price": "309000",
+        "bedrooms\r": "2\r"
+    },
+    {
+        "Year": 2008,
+        "price": "413000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2008,
+        "price": "445000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "600000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "417500",
+        "bedrooms\r": "2\r"
+    },
+    {
+        "Year": 2008,
+        "price": "462500",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "485000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "580000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "380000",
+        "bedrooms\r": "1\r"
+    },
+    {
+        "Year": 2008,
+        "price": "560000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "575000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "1035000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "485000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2008,
+        "price": "599000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "349000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2008,
+        "price": "360000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2008,
+        "price": "375000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2008,
+        "price": "405000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2008,
+        "price": "395000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2008,
+        "price": "417000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2008,
+        "price": "560000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2008,
+        "price": "650000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "416000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "749000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "575000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2008,
+        "price": "625000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2008,
+        "price": "610000",
+        "bedrooms\r": "5\r"
+    },
+    {
+        "Year": 2009,
+        "price": "342500",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2009,
+        "price": "450000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2009,
+        "price": "420000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2009,
+        "price": "394000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2009,
+        "price": "410000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2009,
+        "price": "411500",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2009,
+        "price": "560000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2009,
+        "price": "383000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2009,
+        "price": "385000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2009,
+        "price": "420000",
+        "bedrooms\r": "2\r"
+    },
+    {
+        "Year": 2009,
+        "price": "585000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2009,
+        "price": "590000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2009,
+        "price": "340500",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2009,
+        "price": "410000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2009,
+        "price": "418000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2009,
+        "price": "470000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2009,
+        "price": "609950",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2009,
+        "price": "630000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2009,
+        "price": "750000",
+        "bedrooms\r": "5\r"
+    },
+    {
+        "Year": 2009,
+        "price": "420000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2010,
+        "price": "435000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2010,
+        "price": "712000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2010,
+        "price": "435000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2010,
+        "price": "1350000",
+        "bedrooms\r": "5\r"
+    },
+    {
+        "Year": 2010,
+        "price": "612500",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2010,
+        "price": "420500",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2010,
+        "price": "432600",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2010,
+        "price": "525000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2010,
+        "price": "535000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2010,
+        "price": "640000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2010,
+        "price": "519000",
+        "bedrooms\r": "5\r"
+    },
+    {
+        "Year": 2010,
+        "price": "585000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2010,
+        "price": "372500",
+        "bedrooms\r": "2\r"
+    },
+    {
+        "Year": 2010,
+        "price": "443000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2010,
+        "price": "495000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2010,
+        "price": "580000",
+        "bedrooms\r": "5\r"
+    },
+    {
+        "Year": 2010,
+        "price": "580000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2010,
+        "price": "380000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2010,
+        "price": "400000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2010,
+        "price": "420000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "474000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "410000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "440000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "405100",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "415000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "455000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "568500",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "361500",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "396000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "438000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "465000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "505000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "820000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "439950",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "745000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "445000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "510000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "525000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "529000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "555000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "485000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "500000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "520000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "440000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "465000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2011,
+        "price": "470000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2011,
+        "price": "560000",
+        "bedrooms\r": "5\r"
+    },
+    {
+        "Year": 2012,
+        "price": "456000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "715000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2012,
+        "price": "560000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2012,
+        "price": "425000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "522500",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "430000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "445000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "420000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "555000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2012,
+        "price": "1080000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2012,
+        "price": "412500",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "495000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2012,
+        "price": "586000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2012,
+        "price": "430000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "555000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2012,
+        "price": "650500",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2012,
+        "price": "735000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "751500",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2012,
+        "price": "413300",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "441000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "445000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "460000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2012,
+        "price": "608000",
+        "bedrooms\r": "4\r"
+    },
+    {
+        "Year": 2012,
+        "price": "369000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "409000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "439000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "457000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "570000",
+        "bedrooms\r": "3\r"
+    },
+    {
+        "Year": 2012,
+        "price": "1015000",
+        "bedrooms\r": "5\r"
+    },
+    {
+        "Year": 2012,
+        "price": "527000",
+        "bedrooms\r": "1\r"
+    }
+]
+
+var cat_title = "Bedroom"
+
+function get_rows(data){
+    var key = Object.keys(data[0])[0];
+    var rows = data.map( (value) => value[key]).filter( (value, index, _data) => _data.indexOf(value) == index);
+    rows = rows.filter(function( element ) {
+        return element !== undefined && element !== '' &&  element !== ' ';
+    });
+    return rows;
+}
+
+function get_stats(data){
+    return [Object.keys(data[0])[0], Object.keys(data[0])[1], Object.keys(data[0])[2]];
+}
+
+function get_titles(data){
+    var key = Object.keys(data[0])[2];
+    var titles = data.map( (value) => value[key]).filter( (value, index, _data) => _data.indexOf(value) == index);
+    titles = titles.filter(function( element ) {
+        return element !== undefined && element !== '';
+    });
+    for(var i=0; i<titles.length; i++){
+        titles[i] = titles[i].replace(/["]+/g, '')
+    }
+    return titles;
+}
+
+read_data(data);
+
+var widthO = d3.select('.pieChart2007').node().getBoundingClientRect().width;
+var heightO = widthO;
+
+draw_pie_o(2007)
+draw_pie_o(2008)
+draw_pie_o(2009)
+draw_pie_o(2010)
+draw_pie_o(2011)
+draw_pie_o(2012)
+
+function draw_pie_o(time){
+    //find the data of the given time
+    var compareSet = {
+        "labels": [],
+        "stats": []
+    }
+    for (var i = 0; i < graphData.cols.length; i++)
+    {   
+        var filtered = graphData.cols[i].stats.filter(function(x) { return x.time == time;})
+        // console.log(filtered);
+        if (filtered.length > 0)
+        {
+            if (filtered[0].frequency > 0)
+            {   
+                compareSet.labels.push({title: graphData.cols[i].title, color: graphData.cols[i].color});
+                compareSet.stats.push({label: graphData.cols[i].title, value: filtered[0].frequency, color: graphData.cols[i].color, price: filtered[0].average});
+            }
+        } 
+    }
+
+    var chartO = d3.select(".pieChartSvg"+time)
+    .attr('width',widthO)
+    .attr('height',heightO) 
+
+    var pieO = 
+    d3.pie()
+    .sort(null)
+    .value(function(d) 
+    {
+        return d.value; 
+    });
+
+
+    var arcO = d3.arc()
+    .innerRadius(50)
+    .outerRadius(Math.min(widthO, heightO) / 2);
+
+
+    var arcO = chartO.selectAll('.arc'+time)
+    .data(pieO(compareSet.stats))
+    .enter()
+    .append("g")
+    .append('path')
+    .classed('arc'+time,true)
+    .attr('transform', 'translate(' + widthO/2 +  ',' + heightO/2 +')')
+    .attr('d',arcO)
+    .style('fill', function(d) {
+        return d.data.color;
+    })
+
+    arcO
+    .on("mouseover", function (event, d) {
+        d3.select("#tooltip")
+        .style("left", event.pageX-width/4 + "px")
+        .style("top", event.pageY-width/4 + "px")
+        .style("opacity", 1)
+        .select("#value")
+        d3.select("#tooltip").html(
+                    "Title: " + (d.data.label) + " " + cat_title
+                    + "<br/>" + "Percentage: " + (d.value/sumA).toFixed(2)*100 + "% (" + d.value + ")"
+                    + "<br/>" + "time: " + time
+                    + "<br/>" + "Value: " + d.data.price)
+    })
+    .on("mouseout", function () {
+        // Hide the tooltip
+            d3.select("#tooltip")
+            .style("opacity", 0);
+    })
+
+    chartO
+    .append("g")
+    .attr("transform","translate("+widthO/2 +","+(heightO+10)/2+")")
+    .append("text")
+    .classed("currTimeLabel", true)
+    .attr("text-anchor", "middle")
+    .text(time);
+}

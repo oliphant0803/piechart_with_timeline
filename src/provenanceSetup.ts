eab7198ec68f0b50aa91bde76dc02690de62cafd
Oliver Huang <oliver.h0803@gmail.com>
2023-02-22 16:43:00 -0500

    test no timeout

--- src/provenanceSetup.ts
+++ src/provenanceSetup.ts
@@ -242,5 +242,7 @@ document.onkeydown = function(e){
   }
 }
 
-let n: number;
-n = window.setTimeout(function () { chartInit()  }, 10000);
+chartInit();
+
+// let n: number;
+// n = window.setTimeout(function () { chartInit()  }, 10000);
